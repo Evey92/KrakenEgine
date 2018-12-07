3a55925981e9f25151343283b826b571eaa45ff8
Evey92 <evey9207@gmail.com>
2018-12-07 04:19:15 -0600

    ewdcwq

--- kraUtilities_UnitTest/main.cpp
+++ kraUtilities_UnitTest/main.cpp
@@ -8,22 +8,52 @@
 
 using namespace kraEngineSDK;
 
-void main(int argc, char **argv)
+struct Testing : public ::testing::Test {
+  virtual void SetUp() {
+    fails = 0;
+  }
+
+  virtual void TearDown() {
+    if (fails > 0) {
+      std::cerr << "Testing::TearDown sees failures" << std::endl;
+    }
+  }
+
+  unsigned fails;
+};
+
+int main(int argc, char **argv)
 {
-  Vector4 myVec(3,2, 2, 3);
+  
   ::testing::InitGoogleTest(&argc, argv);
-  std::cout << RUN_ALL_TESTS();
-  std::cout <<"x: " << myVec.x << " Y: " << myVec.y << std::endl;
-  //_getch();
+  return RUN_ALL_TESTS();
 
   
 }
 
-TEST(Utilities, Basic_Test)
+TEST_F(Testing, Basic_Test)
 {
   Vector2 vec1(2, 3);
   Vector2 vec2(2, 4);
   EXPECT_EQ(kraMath::cos(0), 1);
   EXPECT_EQ((vec1 + vec2), Vector2(4, 7));
+  fails += ::testing::Test::HasFailure();
+}
 
+TEST_F(Testing, Basic_Types)
+{
+  EXPECT_TRUE(sizeof(int8) == 1);
+  EXPECT_TRUE(sizeof(int16) == 2);
+  EXPECT_TRUE(sizeof(int32) == 4);
+  EXPECT_TRUE(sizeof(int64) == 8);
+
+
+  EXPECT_TRUE(sizeof(uint8) == 1);
+  EXPECT_TRUE(sizeof(uint16) == 2);
+  EXPECT_TRUE(sizeof(uint32) == 4);
+  EXPECT_TRUE(sizeof(uint64) == 8);
+
+  EXPECT_TRUE(sizeof(float) == 4);
+  EXPECT_TRUE(sizeof(double) == 8);
+  fails += ::testing::Test::HasFailure();
 }
