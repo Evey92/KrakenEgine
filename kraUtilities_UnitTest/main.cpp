078de194bd5b9599b7121308b98ff50da1bc1d28
Evey92 <evey9207@gmail.com>
2018-12-07 04:10:19 -0600

    AEDCX

--- kraUtilities_UnitTest/main.cpp
+++ kraUtilities_UnitTest/main.cpp
@@ -8,14 +8,14 @@
 
 using namespace kraEngineSDK;
 
-int main(int argc, char **argv)
+void main(int argc, char **argv)
 {
   Vector4 myVec(3,2, 2, 3);
   ::testing::InitGoogleTest(&argc, argv);
   std::cout << RUN_ALL_TESTS();
   std::cout <<"x: " << myVec.x << " Y: " << myVec.y << std::endl;
   //_getch();
-	return 0;
+
   
 }
 
