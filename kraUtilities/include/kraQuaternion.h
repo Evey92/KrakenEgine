ebcd4f8e072eef04ac46111d18b59054f511249f
Evey92 <evey9207@gmail.com>
2018-12-07 02:59:55 -0600

    Fixed some stuff

---
+++ kraUtilities/include/kraQuaternion.h
@@ -0,0 +1,8 @@
+#pragma once
+class kraQuaternion
+{
+public:
+  kraQuaternion();
+  ~kraQuaternion();
+};
+
---
+++ kraUtilities/src/kraQuaternion.cpp
@@ -0,0 +1,12 @@
+#include "kraQuaternion.h"
+
+
+
+kraQuaternion::kraQuaternion()
+{
+}
+
+
+kraQuaternion::~kraQuaternion()
+{
+}
